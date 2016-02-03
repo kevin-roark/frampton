c29fb5243547dafee3a1a80ef0334f703364a134
Kevin Roark <ker2143@columbia.edu>
2016-02-02 20:17:40 -0500

    test-driven-development idea for continous playback

---
+++ continous_frampton.js
@@ -0,0 +1,39 @@
+
+var kt = require('kutility');
+var frampton = require('frampton');
+
+var mediaFilepath = './media/';
+
+var videoIDs = kt.shuffle(frampton.util.videoIDsInPath(mediaFilepath));
+
+var currentIDChoices = [];
+
+var segments = [];
+videoIDs.forEach(function(id) {
+  var segment = new frampton.Segment({
+    videoID: id,
+    onComplete: function() {
+      // refill choices if necessary
+      if (currentIDChoices.length === 0) {
+        currentIDChoices = kt.shuffle(videoIDs);
+      }
+
+      var newID = currentIDChoices.shift();
+      segment.setVideoID(newID);
+    }
+  });
+
+  segments.push(segment);
+});
+
+var leaderSegment = new frampton.Segment({
+  childSegments: segments,
+  loop: true
+});
+
+var renderer = new frampton.Renderer({
+  segment: leaderSegment,
+  mediaFilepath: mediaFilepath
+});
+
+renderer.render();
