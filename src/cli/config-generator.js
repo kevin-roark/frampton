4519da889b026b178d77674821c5bfb9f555d044
Kevin Roark <ker2143@columbia.edu>
2016-02-13 21:58:16 -0500

    duration in config via ffprobe

--- src/cli/config-generator.js
+++ src/cli/config-generator.js
@@ -2,10 +2,11 @@
 
 var fs = require('fs');
 var path = require('path');
+var probe = require('node-ffprobe');
 
 var args = process.argv.slice(2);
 
-var outputFilepath = args.length > 1 ? args[2] : './media_config.json';
+var outputFilepath = args.length > 1 ? args[1] : './media_config.json';
 
 var config = {
   path: args.length > 0 ? args[0] : './media',
@@ -13,14 +14,34 @@ var config = {
 };
 
 var files = filesInPath(config.path);
+var itemsBeingProcessed = 0;
+
 files.forEach(function(file) {
   if (path.extname(file) === '.mp4') {
-    config.videos.push({"filename": file});
+    addVideo(file);
   }
 });
 
-var jsonConfig = JSON.stringify(config);
-fs.writeFileSync(outputFilepath, jsonConfig);
+function addVideo(file) {
+  itemsBeingProcessed += 1;
+  probe(path.join(config.path, file), function(err, probeData) {
+    config.videos.push({
+      filename: file,
+      duration: probeData && probeData.streams ? probeData.streams[0].duration : 0.0
+    });
+
+    itemsBeingProcessed -= 1;
+    if (itemsBeingProcessed === 0) {
+      writeToFile();
+    }
+  });
+}
+
+function writeToFile() {
+  var jsonConfig = JSON.stringify(config);
+  fs.writeFileSync(outputFilepath, jsonConfig);
+  console.log('generated config at ' + outputFilepath);
+}
 
 function filesInPath(dir) {
   var files = [];
