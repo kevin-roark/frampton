62e20663a330d97c128b35f98da1238aa93355f1
Kevin Roark <ker2143@columbia.edu>
2016-02-19 21:17:25 -0600

    3000k for now

--- src/cli/web-mp4-convert.sh
+++ src/cli/web-mp4-convert.sh
@@ -3,4 +3,4 @@
 infile=$1
 outfile=$2
 
-ffmpeg -i $infile -codec:v libx264 -profile:v main -preset medium -b:v 4000k -maxrate 4000k -bufsize 8000k -vf scale=-1:720 -threads 0 -b:a 128k $outfile
+ffmpeg -i $infile -codec:v libx264 -profile:v main -preset medium -b:v 3000k -maxrate 3000k -bufsize 6000k -vf scale=-1:720 -threads 0 -b:a 128k $outfile
