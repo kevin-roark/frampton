8b289cafd4f6a157809f2ace29cdcd9d05f76af5
bfasenfest <aowbackup@gmail.com>
2016-03-05 17:08:43 -0500

    very small changes
    may not be necessary

--- src/cli/shot-splitter.js
+++ src/cli/shot-splitter.js
@@ -18,7 +18,8 @@ if (args.length < 2) {
 var srtFilepath = args[0];
 var videoFilepath = args[1];
 var outputFilepath = args.indexOf('--out') > 0 ? args[args.indexOf('--out') + 1] : './out';
-var fps = args.indexOf('--fps') > 0 ? parseFloat(args[args.indexOf('--fps')]) : 24;
+var fps = args.indexOf('--fps') > 0 ? parseFloat(args[args.indexOf('--fps') + 1]) : 24;
+var offset = args.indexOf('--offset') > 0 ? parseFloat(args[args.indexOf('--offset') + 1]) : 0;
 
 var subtitles = fs.readFileSync(srtFilepath).toString().replace(/\./g, ',');
 var subtitleParser = new Subtitle(subtitles);
@@ -33,25 +34,30 @@ var msPerFrame = 41;
 var shotDataArray = Object.keys(shotData);
 var lastIdx = shotDataArray.length - 1;
 
+var startMutliplier = 3;
+
+
 shotData.forEach(function(shot, idx) {
 
   var outfile = path.join(outputFilepath, `${shot.index}.mp4`);
 
+
+
   var start, duration;
   if (idx === 0) {
-    start = (shot.start - (3 * msPerFrame))  / 1000;
-    duration = shot.duration / 1000;
+    start = (shot.start - (startMutliplier * msPerFrame))  / 1000;
+    duration = (shot.duration) / 1000;
   }
   else if (idx === 1) {
-    start = (shot.start - (4 * msPerFrame))  / 1000;
+    start = (shot.start - ((startMutliplier + 1) * msPerFrame))  / 1000;
     duration = (shot.duration + (3 * msPerFrame)) / 1000;
   }
   else if (idx === lastIdx ) {
-    start = (shot.start - (3 * msPerFrame))  / 1000;
-    duration = (shot.duration + (4 * msPerFrame)) / 1000;
+    start = (shot.start - (startMutliplier * msPerFrame))  / 1000;
+    duration = (shot.duration + (3 * msPerFrame)) / 1000;
   }
   else {
-    start = Math.max(0, (shot.start - (3 * msPerFrame)) / 1000);
+    start = Math.max(0, (shot.start - (startMutliplier * msPerFrame)) / 1000);
     duration = (shot.duration + (2 * msPerFrame)) / 1000;
   }
 
