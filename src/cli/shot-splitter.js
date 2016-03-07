d9b9ae75803b40da7bdb3b071e20a63bd144f0e2
Kevin Roark <ker2143@columbia.edu>
2016-03-07 00:30:20 -0500

    --premiere command line option in shot-splitter

--- src/cli/shot-splitter.js
+++ src/cli/shot-splitter.js
@@ -19,7 +19,7 @@ var srtFilepath = args[0];
 var videoFilepath = args[1];
 var outputFilepath = args.indexOf('--out') > 0 ? args[args.indexOf('--out') + 1] : './out';
 var fps = args.indexOf('--fps') > 0 ? parseFloat(args[args.indexOf('--fps') + 1]) : 24;
-var offset = args.indexOf('--offset') > 0 ? parseFloat(args[args.indexOf('--offset') + 1]) : 0;
+var cutForPremiere = args.indexOf('--premiere') > 0;
 
 var subtitles = fs.readFileSync(srtFilepath).toString().replace(/\./g, ',');
 var subtitleParser = new Subtitle(subtitles);
@@ -33,10 +33,10 @@ var commandsRunning = 0;
 var commandQueue = [];
 
 var msPerFrame = 41;
-var startMutliplier = 2; //premiere value is 3
+var startMutliplier = cutForPremiere ? 3 : 2;
+var durationMultiplier;
 
 shotData.forEach(function(shot, idx) {
-
   var outfile = path.join(outputFilepath, `${shot.index}.mp4`);
 
   var start, duration;
@@ -45,16 +45,18 @@ shotData.forEach(function(shot, idx) {
     duration = (shot.duration) / 1000;
   }
   else if (idx === 1) {
-    start = (shot.start - ((startMutliplier )  * msPerFrame))  / 1000;
+    start = (shot.start - (startMutliplier  * msPerFrame))  / 1000;
     duration = (shot.duration + (2 * msPerFrame)) / 1000;
   }
   else if (idx === shotData.length - 1) {
     start = (shot.start - (startMutliplier * msPerFrame))  / 1000;
-    duration = (shot.duration + (2 * msPerFrame)) / 1000; //premiere value is 3
+    durationMultiplier = cutForPremiere ? 3 : 2;
+    duration = (shot.duration + (durationMultiplier * msPerFrame)) / 1000;
   }
   else {
     start = Math.max(0, (shot.start - (startMutliplier * msPerFrame)) / 1000);
-    duration = (shot.duration + (1 * msPerFrame)) / 1000; //premiere value is 2
+    durationMultiplier = cutForPremiere ? 2 : 1;
+    duration = (shot.duration + (durationMultiplier * msPerFrame)) / 1000;
   }
 
   var command = `ffmpeg -ss ${start} -t ${duration} -i ${videoFilepath} -c:v libx264 ${outfile}`;
