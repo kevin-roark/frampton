99fafaad2bc68f1e40697e59640c015c9c71cb2f
bfasenfest <GEMPA@dyn-160-39-171-209.dyn.columbia.edu>
2016-03-14 13:46:33 -0400

    fixing typos and adding back premiere flag

--- src/cli/shot-splitter.js
+++ src/cli/shot-splitter.js
@@ -20,7 +20,7 @@ var videoFilepath = args[1];
 var outputFilepath = args.indexOf('--out') > 0 ? args[args.indexOf('--out') + 1] : './out';
 var fps = args.indexOf('--fps') > 0 ? parseFloat(args[args.indexOf('--fps') + 1]) : 24;
 var cutForPremiere = args.indexOf('--premiere') > 0;
-var offset = args.indexOf('--offset') > 0 ? parseFloat(args[args.indexOf('--offset') + 1]) : 0;
+var offset = args.indexOf('--offset') > 0 ? parseFloat(argcs[args.indexOf('--offset') + 1]) : 0;
 var firstIdxMultiplier = args.indexOf('--start') > 0 ? parseFloat(args[args.indexOf('--start') + 1]) : 2; // values less than 2 trim the begining
 var lastIdxMultiplier = args.indexOf('--end') > 0 ? parseFloat(args[args.indexOf('--end') + 1]) : 2; // values less than 2 trim the end
 
@@ -39,8 +39,13 @@ var commandQueue = [];
 var shotDataArray = Object.keys(shotData);
 var lastIdx = shotDataArray.length - 1;
 var msPerFrame = 41;
-var startMutliplier = cutForPremiere ? 3 : 2;
+var startMultiplier = cutForPremiere ? 3 : 2;
+var normalMultiplier = cutForPremiere ? 2 : 1;
 var durationMultiplier;
+
+if(cutForPremiere){
+  lastIdxMultiplier = 3;
+}
 
 shotData.forEach(function(shot, idx) {
 
@@ -48,20 +53,20 @@ shotData.forEach(function(shot, idx) {
 
   var start, duration;
   if (idx === 0) {
-    start = (shot.start - (firstIdxMutliplier * msPerFrame))  / 1000;
+    start = (shot.start - (firstIdxMultiplier * msPerFrame))  / 1000;
     duration = (shot.duration) / 1000;
   }
   else if (idx === 1) {
-    start = (shot.start - ((startMutliplier )  * msPerFrame))  / 1000;
+    start = (shot.start - ((startMultiplier )  * msPerFrame))  / 1000;
     duration = (shot.duration + (2 * msPerFrame)) / 1000;
   }
   else if (idx === lastIdx) {
-    start = (shot.start - (startMutliplier * msPerFrame))  / 1000;
+    start = (shot.start - (startMultiplier * msPerFrame))  / 1000;
     duration = (shot.duration + (lastIdxMultiplier * msPerFrame)) / 1000; //premiere value is 3
   }
   else {
-    start = Math.max(0, (shot.start - (startMutliplier * msPerFrame)) / 1000);
-    duration = (shot.duration + (1 * msPerFrame)) / 1000; //premiere value is 2
+    start = Math.max(0, (shot.start - (startMultiplier * msPerFrame)) / 1000);
+    duration = (shot.duration + (normalMultiplier * msPerFrame)) / 1000; //premiere value is 2
   }
 
   var command = `ffmpeg -ss ${start} -t ${duration} -i ${videoFilepath} -c:v libx264 ${outfile}`;
