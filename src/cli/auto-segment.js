d652cb15558f661f464533fac1a1f97101481b00
Kevin Roark <ker2143@columbia.edu>
2016-03-14 11:58:54 -0400

    theoretical shot-splitter implementation

--- src/cli/auto-segment.js
+++ src/cli/auto-segment.js
@@ -11,12 +11,7 @@ var mediaPath = args.length > 0 ? args[0] : './media';
 var commandsRunning = 0;
 var commandQueue = [];
 
-var config = {
-  path: mediaPath,
-  videos: []
-};
-
-var files = filesInPath(config.path);
+var files = filesInPath(mediaPath);
 
 files.forEach(function(file) {
   if (path.extname(file) === '.mp4') {
@@ -26,13 +21,20 @@ files.forEach(function(file) {
 
 
 function segmentVideo(file) {
-  var videoPath = path.join(config.path, file);
+  var videoPath = path.join(mediaPath, file);
 
-  var command = `video_segmentation ${videoPath}`;
-  run(command);
+  var segmentCommand = `video_segmentation ${videoPath}`;
+  run(segmentCommand, function() {
+    var shotSplitterPath = path.join(__dirname, 'shot-splitter.js');
+    var srtPath = ''; // TODO: I don't know because I don't know what video_segmentation produces :/
+    var outPath = path.join(mediaPath, 'split-videos');
+
+    var shotSplitCommand = `node ${shotSplitterPath} ${srtPath} ${videoPath} --out ${outPath}`;
+    run(shotSplitCommand);
+  });
 }
 
-function run(command) {
+function run(command, callback) {
   if (commandsRunning > 1) {
     commandQueue.push(command);
     return;
@@ -48,6 +50,10 @@ function run(command) {
     if (commandQueue.length > 0) {
       var nextCommand = commandQueue.shift();
       run(nextCommand);
+    }
+
+    if (callback) {
+      callback();
     }
   });
 }
