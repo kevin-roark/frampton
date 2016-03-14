0e79c37c05b48e9fcbf6cbb8afcfce40658893f5
bfasenfest <aowbackup@gmail.com>
2016-03-14 16:41:40 -0400

    fixing split to run for all videos and revising outpath and flags

--- src/cli/auto-segment.js
+++ src/cli/auto-segment.js
@@ -8,7 +8,7 @@ var args = process.argv.slice(2);
 
 var mediaPath = args.length > 0 ? args[0] : './media';
 var splitShots = args.indexOf('--split') > 0;
-var splitOnly = args.indexOf('--noseg') > 0;
+var splitOnly = args.indexOf('--splitonly') > 0;
 var startFlag = args.indexOf('--start') > 0 ? parseFloat(args[args.indexOf('--start') + 1]) : 2; // values less than 2 trim the begining
 var endFlag = args.indexOf('--end') > 0 ? parseFloat(args[args.indexOf('--end') + 1]) : 2; // values less than 2 trim the end
 
@@ -25,29 +25,34 @@ files.forEach(function(file) {
 });
 
 
-function segmentVideo(file) {
-  var videoPath = path.join(mediaPath, file);
-
-  var segmentCommand = `video_segmentation ${videoPath}`;
-
-  if(splitOnly) {
-    segmentCommand = '';
+files.forEach(function(file) {
+  if (path.extname(file) === '.mp4') {
+    splitVideo(file);
   }
+});
 
-  run(segmentCommand, function() {
-    var shotSplitterPath = path.join(__dirname, 'shot-splitter.js');
-    var srtPath = videoPath.substr(0, videoPath.lastIndexOf(".")) +  '_shots.srt';
-    var outPath = path.join(mediaPath, file.substr(0, file.lastIndexOf(".")));
-    var makeDirectoryCommand = `md ${outPath}`;
-    var shotSplitCommand = `node ${shotSplitterPath} ${srtPath} ${videoPath} --out ${outPath} --start ${startFlag} --end ${endFlag}`;
 
-    if (splitShots || splitOnly){
-      run(makeDirectoryCommand);
-      run(shotSplitCommand);
-    }
-
-  });
+function segmentVideo(file) {
+  var videoPath = path.join(mediaPath, file)
+  var segmentCommand = `video_segmentation ${videoPath}`;
+  if(splitOnly){segmentCommand = ''; }
+  run(segmentCommand);
 }
+
+function splitVideo(file) {
+  var videoPath = path.join(mediaPath, file)
+  var shotSplitterPath = path.join(__dirname, 'shot-splitter.js');
+  var srtPath = videoPath.substr(0, videoPath.lastIndexOf(".")) +  '_shots.srt';
+  var outPath = path.join(mediaPath, 'split-scenes', file.substr(0, file.lastIndexOf(".")));
+  var makeDirectoryCommand = `md ${outPath}`;
+  var shotSplitCommand = `node ${shotSplitterPath} ${srtPath} ${videoPath} --out ${outPath} --start ${startFlag} --end ${endFlag}`;
+
+  if (splitShots || splitOnly){
+    run(makeDirectoryCommand);
+    run(shotSplitCommand);
+  }
+}
+
 
 function run(command, callback) {
   if (commandsRunning > 1) {
