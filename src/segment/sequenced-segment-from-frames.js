0ea7695806132238ae76f2d5ec3a690431afd611
Kevin Roark <ker2143@columbia.edu>
2016-04-07 00:21:29 -0400

    experimental image segment variation of sequenced-segment-from-frames

--- src/segment/sequenced-segment-from-frames.js
+++ src/segment/sequenced-segment-from-frames.js
@@ -1,10 +1,12 @@
 
 var VideoSegment = require('./video-segment');
+var ImageSegment = require('./image-segment');
 var SequencedSegment = require('./sequenced-segment');
 
 module.exports = function sequencedSegmentFromFrames(framesData, options={}) {
   var firstFrameIndex = options.firstFrameIndex || 0;
   var numberOfFrames = options.numberOfFrames || framesData.frames.length;
+  var cutVideos = options.cutVideos || false;
 
   var frameDuration = 1 / framesData.fps;
 
@@ -13,12 +15,22 @@ module.exports = function sequencedSegmentFromFrames(framesData, options={}) {
   for (var i = firstFrameIndex; i < numberOfFrames; i++) {
     var frame = framesData.frames[i];
 
-    var videoSegment = new VideoSegment(framesData);
-    videoSegment
-      .setStartTime(frame.timecode)
-      .setDuration(frameDuration);
+    if (cutVideos) {
+      var videoSegment = new VideoSegment(framesData);
+      videoSegment
+        .setStartTime(frame.timecode)
+        .setDuration(frameDuration);
 
-    segments.push(videoSegment);
+      segments.push(videoSegment);
+    }
+    else {
+      var imageSegment = new ImageSegment({
+        filename: frame.imageFilename,
+        duration: frameDuration
+      });
+
+      segments.push(imageSegment);
+    }
   }
 
   // put segments in given options array to allow arbitrary options-passing to SequencedSegment
